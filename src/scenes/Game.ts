fd285e0da36e7f5a2b0d3bab51290092cfb8a815
Edward Zhang <edwardz123@gmail.com>
2023-11-11 20:50:12 -0800

    Add combo listener

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,13 +1,17 @@
-import Phaser from 'phaser'
-import { Board } from '~/core/Board'
+import Phaser from 'phaser';
+import { Board } from '~/core/Board';
 
 export class Game extends Phaser.Scene {
-  public board!: Board
-  constructor() {
-    super('game')
-  }
+	public board!: Board;
+	constructor() {
+		super('game');
+	}
 
-  create() {
-    this.board = new Board(this)
-  }
+	create() {
+		this.board = new Board(this);
+		this.board.addComboListener((combo) => {
+			// TODO: calculate damage
+			console.log(combo);
+		});
+	}
 }
