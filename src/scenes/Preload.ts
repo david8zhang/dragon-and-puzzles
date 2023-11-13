6ec8c22429a95fd30ed8c83929b6ba96f56e045a
Edward Zhang <edwardz123@gmail.com>
2023-11-12 19:06:50 -0800

    Use sprites for orbs

--- src/scenes/Preload.ts
+++ src/scenes/Preload.ts
@@ -8,6 +8,14 @@ export class Preload extends Phaser.Scene {
 		this.load.image('fire-dragon-debug', 'charizard.png');
 		this.load.image('water-dragon-debug', 'gyarados.png');
 		this.load.image('gameover', 'gameover.png');
+
+		this.load.image('orb', 'orb.png');
+		this.load.image('orb-red', 'orb-red.png');
+		this.load.image('orb-blue', 'orb-blue.png');
+		this.load.image('orb-green', 'orb-green.png');
+		this.load.image('orb-yellow', 'orb-yellow.png');
+		this.load.image('orb-purple', 'orb-purple.png');
+		this.load.image('orb-pink', 'orb-pink.png');
 	}
 
 	create() {
