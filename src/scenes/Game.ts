ce2666ed56372dac099c8703805e1ebb6ae196c8
David Zhang <david8zhang@berkeley.edu>
2023-11-23 19:04:15 -0800

    tune some enemy health values, fix bugs

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -9,7 +9,7 @@ import { Constants } from '~/utils/Constants'
 
 export class Game extends Phaser.Scene {
   public board!: Board
-  public level: number = 4
+  public level: number = 0
   public player!: Player
   public enemy!: Enemy
 
@@ -131,7 +131,7 @@ export class Game extends Phaser.Scene {
         this.transitionSubtitleText.setVisible(false)
         this.continueButton.setVisible(false)
         this.transitionOrbUnlocked.setVisible(false)
-        if (this.level === ENEMIES.length - 2) {
+        if (this.level === ENEMIES.length - 1) {
           this.game.scene.start('victory', { isPreBoss: true })
         } else {
           this.game.scene.start('game', { level: this.level + 1 })
@@ -184,7 +184,7 @@ export class Game extends Phaser.Scene {
   }
 
   transitionToNextEnemy() {
-    if (this.level === ENEMIES.length - 1) {
+    if (this.level >= ENEMIES.length) {
       this.game.scene.start('victory', { isPreBoss: false })
     } else {
       this.displayTransitionOverlay()
